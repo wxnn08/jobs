501ccfe961e87392b92c85f1c85aeec1ced5e720
wxnn08 <wesleypersilva@gmail.com>
2018-06-24 17:13:20 -0300

    calcula secante de funcao pre estabelecida

--- MetodoSecante/calcSecante.py
+++ MetodoSecante/calcSecante.py
@@ -1,13 +1,20 @@
 def function(x):
+	return x**2
 
 def secant():
-	x=[0, 1]
-	f=[0,1]
+	x=[0.5, 1.0]
+	f=[0.625, -3]
 	for i in range(2,8):
-		xValue = x[i-1]-(f[i-1]*)
-		x.append(xValue);
+		xValue = x[i-1]-((f[i-1]*(x[i-1]-x[i-2]))/(f[i-1]-f[i-2]))
+		x.append(xValue)
+		fValue = function(xValue)
+		f.append(fValue)
+	
+	for i in range(0,8):
+		print("x = {}| f = {}".format(x[i],f[i]))
 
 def main():
 	secant()
+
 if __name__ == "__main__":
     main()
