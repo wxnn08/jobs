3eb82575c9fa5a8cefc181d318318c3bacdca151
wxnn08 <wesleypersilva@gmail.com>
2018-06-24 16:51:02 -0300

    move

---
+++ MetodoSecante/2.py
@@ -0,0 +1,13 @@
+def function(x):
+
+def secant():
+	x=[0, 1]
+	f=[0,1]
+	for i in range(2,8):
+		xValue = x[i-1]-(f[i-1]*)
+		x.append(xValue);
+
+def main():
+	secant()
+if __name__ == "__main__":
+    main()
---
+++ MetodoSecante/secante.py
@@ -0,0 +1,3 @@
+for x in range(-100, 100):
+	y = 3*x + 7
+	print(y)
