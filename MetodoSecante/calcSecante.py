9586f1648f9bf93d224053e2ec64b2e4069e5c02
wxnn08 <wesleypersilva@gmail.com>
2018-06-24 23:17:25 -0300

    Apresenta erros/Calcula mesmo com problemas

--- MetodoSecante/calcSecante.py
+++ MetodoSecante/calcSecante.py
@@ -18,36 +18,41 @@ def secant(x, f, it, TAM):
 		for i in range (1, len(f)):
 			fSec(i, x[i-1], x[i], f[i-1], f[i], TAM)
 		
-		
 	except ZeroDivisionError:
 		print("ERRO #01: Divisao por zero")
 	except TypeError:
-		print("TypeError")
+		print("ERRO #02: TypeError")
+		raiz = "?"
 
-	plt.grid(True)
+	if raiz!="?":
+		raiz = str(x[len(x)-1])
+
 	funcao = "x^2"
-	plt.title("{}, raiz = ".format(funcao) + str(x[len(x)-1]))
+	plt.title("{}, raiz = ".format(funcao) + raiz)
 	plt.ylabel("f(x)")
 	plt.xlabel("x")
 	plt.legend()
+	plt.grid(True)
 	plt.show()
 
 def fSec(i, x0, x1, f0, f1, TAM):
-	if x1-x0==0:
-		return 
-	m = (f1-f0)/(x1-x0)
-	xAxis = [x1]
-	yAxis = [f1]
-	for x in range(int(round(x1))-TAM, int(round(x1))+TAM):
-		y = m*(x-xAxis[0])+yAxis[0]
-		xAxis.append(x)
-		yAxis.append(y)
-	lbl = str(i)+" iteração"
-	plt.plot(xAxis, yAxis, label = lbl)
+	try:
+		m = (f1-f0)/(x1-x0)
+		xAxis = [x1]
+		yAxis = [f1]
+		for x in range(int(round(x1))-TAM, int(round(x1))+TAM):
+			y = m*(x-xAxis[0])+yAxis[0]
+			xAxis.append(x)
+			yAxis.append(y)
+		lbl = str(i)+" iteração"
+		plt.plot(xAxis, yAxis, label = lbl)
+
+	except ZeroDivisionError:
+		print("ERRO #01: Divisao por zero")
 
 def fx(x):
 	try:
-		return -9*(x**2)-27*x+5
+		return 1/x
 	except ZeroDivisionError:
 		print("ERRO #01: Divisao por zero")
 
