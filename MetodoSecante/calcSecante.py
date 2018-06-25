bd9a2ce1ed2122b89d07e437c62d3813c248ac8c
wxnn08 <wesleypersilva@gmail.com>
2018-06-24 23:07:06 -0300

    Plota mesmo com alguns erros de divisao por zero e falta de raiz

--- MetodoSecante/calcSecante.py
+++ MetodoSecante/calcSecante.py
@@ -8,22 +8,22 @@ def secant(x, f, it, TAM):
 		yAxis.append(fx(i))
 	plt.plot(xAxis, yAxis, )
 
-	for i in range(2,it):
-		if f[i-1]-f[i-2] == 0:
-			break
-		else:
-			if x[i-1]-x[i-2] == 0:
-				break
-
-			else:
-				xValue = x[i-1]-((f[i-1]*(x[i-1]-x[i-2]))/(f[i-1]-f[i-2]))
+	try:
+		for i in range(2,it):
+			xValue = x[i-1]-((f[i-1]*(x[i-1]-x[i-2]))/(f[i-1]-f[i-2]))
 			x.append(xValue)
 			fValue = fx(xValue)
 			f.append(fValue)
-	
-	for i in range (1, len(f)):
-		fSec(i, x[i-1], x[i], f[i-1], f[i], TAM)
-	
+
+		for i in range (1, len(f)):
+			fSec(i, x[i-1], x[i], f[i-1], f[i], TAM)
+		
+		
+	except ZeroDivisionError:
+		print("ERRO #01: Divisao por zero")
+	except TypeError:
+		print("TypeError")
+
 	plt.grid(True)
 	funcao = "x^2"
 	plt.title("{}, raiz = ".format(funcao) + str(x[len(x)-1]))
@@ -33,8 +33,9 @@ def secant(x, f, it, TAM):
 	plt.show()
 
 def fSec(i, x0, x1, f0, f1, TAM):
+	if x1-x0==0:
+		return 
 	m = (f1-f0)/(x1-x0)
-	#y = m(x-x0)+y0
 	xAxis = [x1]
 	yAxis = [f1]
 	for x in range(int(round(x1))-TAM, int(round(x1))+TAM):
@@ -45,16 +46,16 @@ def fSec(i, x0, x1, f0, f1, TAM):
 	plt.plot(xAxis, yAxis, label = lbl)
 
 def fx(x):
-	return x**2
+	try:
+		return -9*(x**2)-27*x+5
+	except ZeroDivisionError:
+		print("ERRO #01: Divisao por zero")
 
 def main():
-	# Pré valores passados pelo usuario
-	x=[0.5, 1.0]		# x0, x1
-	f=[0.625, -3.0]		# f0, f1
-	it = 7				#numero de iteracoes
-	TAM = 100			#Tamanho para plotar o gráfico, é necessario encontrar um valor melhor
-
-	# Chama função secant() para calcular e plotar o gráfico #
+	x=[0, 1]
+	f=[fx(x[0]), fx(x[1])]
+	it = 7
+	TAM = 100
 	secant(x, f, it+1, TAM)
 
 
