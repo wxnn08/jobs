f7ffc90a750d095ee1b4b334f3ddab7a819e53f2
wxnn08 <wesleypersilva@gmail.com>
2018-06-29 12:36:52 -0300

    add decimal_range/interactive graph

--- Newton-Secante/calc.py
+++ Newton-Secante/calc.py
@@ -1,4 +1,12 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import math
+import time
+
+def decimal_range(start, stop, increment):
+    while start < stop: # and not math.isclose(start, stop): Py>3.5
+        yield start
+        start += increment
 
 def biseccao (x, it):
 	print("# Bisecção")
@@ -22,11 +30,10 @@ def secante(x, f, it, eps, plotRange):
 	plotLeft = int(x[0]-plotRange)
 	plotRight = int(x[1]+plotRange)
 	funcao = "x^2"
-	root = 0
+	raiz = 0
 
 	try:
 		plotFuncaoPrincipal(x, funcao, plotLeft, plotRight)
-
 		cont = 2 
 		while cont<it and abs(fx(len(x)-1))>eps and f[cont-1]!=f[cont-2]:
 			xValue = x[cont-1]-((f[cont-1]*(x[cont-1]-x[cont-2]))/(f[cont-1]-f[cont-2]))
@@ -40,54 +47,66 @@ def secante(x, f, it, eps, plotRange):
 
 	except TypeError:
 		print("ERRO #02: TypeError")
-		root = "?"
+		raiz = "?"
 
-	for i in range (2, len(f)):
+	for i in range (1, len(f)):
 		if x[i]!=x[i-1]:
 			plotSecante(i, x[i-1], x[i], f[i-1], f[i], plotLeft, plotRight)
 
-	if root!="?":
-		root = str(x[len(x)-1])
+	if raiz!="?":
+		raiz = str(x[len(x)-1])
 
-	plt.title("{}, raiz = ".format(funcao) + root)
-	plt.ylabel("f(x)")
-	plt.xlabel("x")
-	plt.legend()
-	plt.grid(True)
-	plt.show()
-
+	plt.title("{}, raiz = ".format(funcao) + raiz)
+	
 def plotFuncaoPrincipal(x, nome, plotLeft, plotRight):
 	xAxis = []
 	yAxis = []
-	for i in range(plotLeft, plotRight):
+	for i in decimal_range(plotLeft, plotRight, 0.01):
 		yValue = fx(i)
 		if yValue!="?":
 			xAxis.append(i)
 			yAxis.append(yValue)
 
 	plt.plot(xAxis, yAxis,"r", label=nome)
+	plt.draw()
+	plt.waitforbuttonpress()
 
 def plotSecante(i, x0, x1, f0, f1, plotLeft, plotRight):
 	try:
 		print("---- x[{}]: {}".format(i, x1))
-		xAxis = [x1]
-		yAxis = [f1]
 		m = (f1-f0)/(x1-x0)
 
-		for x in range(plotLeft, plotRight):
-			y = m*(x-xAxis[0])+yAxis[0]
+		xAxis = []
+		yAxis = []
+		#for x in range(plotLeft, plotRight):
+		for x in decimal_range(x0-x1*0.5, x1+x1*0.5, 0.01):
+			y = m*(x-x1)+f1
 			xAxis.append(x)
 			yAxis.append(y)
+		
+		plt.plot(xAxis, yAxis, linewidth=0.8)
+		plt.draw()
+		plt.waitforbuttonpress()
 
-		lbl = str(i-1)+" iteração"
-		plt.plot(xAxis, yAxis, "--", linewidth=0.8, label = lbl)
+		lbl = str(i)+" iteração"
+		#plt.plot(xAxis, yAxis, linewidth=2, label = lbl)
+
+		xAxis = [x1, x1]
+		yAxis = [0, f1]
+		plt.text(x1, 0, "x"+str(i))
+		plt.plot(xAxis, yAxis, "k--", linewidth=0.8)
+
+		plt.legend()
+		plt.draw()
+		plt.waitforbuttonpress()
+
 
 	except ZeroDivisionError:
 		print("ERRO #03: Divisao por zero")
 
 def fx(x):
 	try:
-		return ((x**2)/10)+x-1000
+		return math.sin(x)
 
 	except ZeroDivisionError:
 		print("ERRO #04: Divisao por zero")
@@ -96,14 +115,19 @@ def fx(x):
 	except TypeError:
 		print("ERRO #05: TypeError")
 		
-
 def main():
+	plt.ion()
+	plt.show()
+	plt.ylabel("f(x)")
+	plt.xlabel("x")
+	plt.grid(True)
+
 	""" Configuracoes passadas pelo usuario """
-	x=[0, 10000]
+	x=[1, 4]
 	itSMax = 100
 	itB = 7
-	plotRange = 100
-	eps = 0.01
+	plotRange = 5 
+	eps = 0.001
 	
 	""" Verifica se existe uma raiz no intervalo fornecido """
 	if fx(x[0])*fx(x[1])<0:
@@ -117,12 +141,13 @@ def main():
 		""" Calcula pelo método das secantes. 
 
 		itSMax = Máximo de iterações realizadas
-		eps = Aproximação 
+		eps = Aproximação pela secante
 		plotRange = Tamanho da plotagem do gráfico """
 		secante(x, f, itSMax+1, eps, plotRange)	
+		plt.waitforbuttonpress()
 
 	else:
-		print("Nao existem raizes no intervalo dado")
+		print("Existem mais de uma ou nenhuma raiz no intervalo dado")
 
 if __name__ == "__main__":
     main()
